2b24698ffd342c9a214d47a7c78f79f49fbd0e0d
Bruce Brown <bruce.brown@logmein.com>
2020-01-09 16:38:51 -0800

    Fix forx11 forwarding
    If unable to create accelerated renderer, create one using defaults.

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -320,7 +320,9 @@ private:
 public:
     explicit Renderer(SDL_Window *win)
             : ren(SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)) {
-
+        if(ren == nullptr) {
+            ren = SDL_CreateRenderer(win, -1, 0);
+        }
     }
 
     ~Renderer() {
