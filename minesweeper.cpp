9e37bf1e4e0afd7ea3a66d589dad554566eb9bc5
Elliot Barlas <elliotbarlas@gmail.com>
2020-01-26 15:38:29 -0800

    move Game class into separate header and cpp files.

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,88 +1,14 @@
 #include <iostream>
-#include <vector>
-#include <memory>
 #include "SDL.h"
 #include "config/Mode.h"
 #include "config/Options.h"
 #include "config/Layout.h"
-#include "util/ClockTimer.h"
-#include "util/Random.h"
-#include "util/Matrix.h"
 #include "sdl/ImageRepo.h"
 #include "sdl/Renderer.h"
 #include "sdl/Window.h"
-#include "sprite/Sprite.h"
-#include "sprite/GameStateListener.h"
-#include "sprite/Timer.h"
-#include "sprite/TileListener.h"
-#include "sprite/FlagStateListener.h"
-#include "sprite/FlagCounter.h"
-#include "sprite/Button.h"
-#include "sprite/Grid.h"
-#include "sprite/Background.h"
+#include "sprite/Game.h"
 
 using namespace minesweeper;
-
-class Game {
-private:
-    Renderer &renderer;
-    std::vector<SpritePtr> sprites;
-
-    void onClick(SDL_MouseButtonEvent evt) {
-        for (auto &sprite : sprites)
-            sprite->onClick(evt);
-    }
-
-    void render() {
-        for (auto &sprite : sprites)
-            sprite->render();
-        renderer.repaint();
-    }
-
-public:
-    Game(ImageRepo &imageRepo, Renderer &renderer, const Options &options, const Layout &layout, Mode::Enum mode)
-            : renderer(renderer) {
-        BackgroundPtr background{std::make_shared<Background>(imageRepo, layout, mode)};
-        TimerPtr timer{std::make_shared<Timer>(imageRepo, layout)};
-        FlagCounterPtr flagCounter{std::make_shared<FlagCounter>(imageRepo, options, layout)};
-        ButtonPtr button{std::make_shared<Button>(imageRepo, options, layout)};
-        GridPtr grid{std::make_shared<Grid>(imageRepo, options, layout)};
-
-        std::vector<GameStateListenerWPtr> gameStateListeners{grid, timer, flagCounter};
-        button->setListeners(gameStateListeners);
-
-        std::vector<TileListenerWPtr> tileRevealListeners{button, flagCounter};
-        grid->setListeners(tileRevealListeners);
-
-        std::vector<FlagStateListenerWPtr> flagStateListeners{grid};
-        flagCounter->setListeners(flagStateListeners);
-
-        sprites.push_back(background);
-        sprites.push_back(timer);
-        sprites.push_back(flagCounter);
-        sprites.push_back(button);
-        sprites.push_back(grid);
-    }
-
-    void run() {
-        render();
-        while (true) {
-            SDL_Event e;
-            int res = SDL_WaitEventTimeout(&e, 100);
-            if (res != 0) {
-                if (e.type == SDL_QUIT) {
-                    break;
-                } else if (e.type == SDL_MOUSEBUTTONDOWN) {
-                    onClick(e.button);
-                    render();
-                }
-            } else {
-                // render on timeout
-                render();
-            }
-        }
-    }
-};
 
 int main(int argc, char **argv) {
     Mode::Enum mode = Mode::parse(argc > 1 ? *argv[1] : 'e');
